0e75ff2b42d060f017749e5eb14061ec93f44e28
ramp07412 <ramp07413@gmail.com>
2025-06-02 22:53:41 +0530

    update chromium

--- puppeteerHelper.js
+++ puppeteerHelper.js
@@ -4,15 +4,15 @@ const chromium = require("chrome-aws-lambda");
 async function openChessLink(url) {
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
+    executablePath: await chromium.executablePath || '/usr/bin/chromium-browser',
+    headless: true,
   });
 
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle2" });
+
   console.log("✅ Page opened in headless mode");
 
-  // Optional: close after some time
   await new Promise(res => setTimeout(res, 5000));
   await browser.close();
 }
